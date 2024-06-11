fe5c043cbe187d3a855a567a83b550ef44952671
Pedro Dias <pedroltdias@gmail.com>
2024-06-10 21:37:26 -0300

    first ocmmit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "urubu_do_pix"
---
+++ src/main/kotlin/br/com/urubudopix/UrubuDoPixApplication.kt
@@ -0,0 +1,11 @@
+package br.com.urubudopix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrubuDoPixApplication
+
+fun main(args: Array<String>) {
+    runApplication<UrubuDoPixApplication>(*args)
+}
